5383a4d643cc5f30f3c17399423662ebc9dc0125
YayL <jonasbeast9@gmail.com>
2026-05-07 17:05:20 +0200

    Added documentation

--- app/Player.hs
+++ app/Player.hs
@@ -20,6 +20,7 @@ drawPlayer :: Player -> Texture -> IO ()
 drawPlayer player texture = do
     let (x, y) = getTile (playerPosition player)
 
+		-- Get head rotation
         rotation =
             case direction player of
                 PlayerUp    -> 0
@@ -27,14 +28,17 @@ drawPlayer player texture = do
                 PlayerDown  -> 180
                 PlayerLeft  -> 270
 
+		-- Get position of player head
         destRect = Rectangle
             (fromIntegral x + fromIntegral tile_size / 2)
             (fromIntegral y + fromIntegral tile_size / 2)
             (fromIntegral tile_size)
             (fromIntegral tile_size)
 
+		-- Where to read in texture
         sourceRect = Rectangle 0 0 32 32
 
+		-- What origin to rotate about
         origin = Vector2
             (fromIntegral tile_size / 2)
             (fromIntegral tile_size / 2)
@@ -45,8 +49,10 @@ drawPlayer player texture = do
 
 movePlayer :: Player -> (Player -> IO Player) -> IO Player
 movePlayer player inputPlayer = do
+	-- Get player key input
 	player' <- inputPlayer player
 	let (x, y) = playerPosition player'
+	-- Update position based on potential new direction
 	return (case direction player' of
 		PlayerUp	-> player' {playerPosition = (x, y - 1)}
 		PlayerDown	-> player' {playerPosition = (x, y + 1)}
@@ -54,6 +60,7 @@ movePlayer player inputPlayer = do
 		PlayerRight	-> player' {playerPosition = (x + 1, y)})
 
 
+-- Map player keybinds to direction for Player1 (WASD Keys)
 inputPlayer1 :: Player -> IO Player
 inputPlayer1 player = do
 	let oldDirection = direction player
@@ -68,6 +75,7 @@ inputPlayer1 player = do
 			 else if pressedD && (oldDirection /= PlayerLeft) then player {direction = PlayerRight}
 			 else player)
 
+-- Map player keybinds to direction for Player2 (Arrow Keys)
 inputPlayer2 :: Player -> IO Player
 inputPlayer2 player = do
 	let oldDirection = direction player
@@ -82,6 +90,7 @@ inputPlayer2 player = do
 			 else if pressedRight && (oldDirection /= PlayerLeft) then player {direction = PlayerRight}
 			 else player)
 			
+-- Quick coordinate check to see if coordinates are out of bounds
 isOutOfBounds :: Position -> Bool
 isOutOfBounds (x, y) =
     x <= 0 || x >= tiles_W - 1 ||
